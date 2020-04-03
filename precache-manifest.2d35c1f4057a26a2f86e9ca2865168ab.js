3a476893c50587bb718b9fa56f70be3bf7b5593c
Erik Raith <raithhtiar@gmail.com>
2020-04-03 10:14:14 -0600

    Updates

---
+++ precache-manifest.2d35c1f4057a26a2f86e9ca2865168ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9b04ecb1f8113e434c2c07dfc7dc46a",
+    "url": "/VRAD/index.html"
+  },
+  {
+    "revision": "4aefbca17b9f5f050066",
+    "url": "/VRAD/static/css/main.0d21c28f.chunk.css"
+  },
+  {
+    "revision": "23d8f691f9aaff3626d1",
+    "url": "/VRAD/static/js/2.3d1f592b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/VRAD/static/js/2.3d1f592b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aefbca17b9f5f050066",
+    "url": "/VRAD/static/js/main.4500b359.chunk.js"
+  },
+  {
+    "revision": "8bd83e833918bb1837e4",
+    "url": "/VRAD/static/js/runtime-main.8f54255f.js"
+  }
+]);
